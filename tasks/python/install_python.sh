f347b1c81dd3537fa266ce130b012de18270d246
Grant-Steinfeld <gsteinfeld@yahoo.com>
2019-10-25 03:56:18 -0400

    added ability maybe to install py 2 and 3 simul and setup venvs

--- tasks/python/install_python.sh
+++ tasks/python/install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 logger start_py_install
-export HOMEDIR=/opt/lastlist/apps
+export HOMEDIR=/opt/bchlabs/apps
 
-cd /opt/lastlist/apps/setup/python
+cd /opt/bchlabs/apps/setup/python2
 logger about to download python tar file
 
 wget --no-check-certificate https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tar.xz
@@ -12,9 +12,9 @@ xz -d Python-2.7.8.tar.xz
 tar -xvf Python-2.7.8.tar
 
 cd Python-2.7.8
-export LD_RUN_PATH=$HOMEDIR/python/py278/lib
-./configure --prefix=$HOMEDIR/python/py278 --enable-shared
+export LD_RUN_PATH=$HOMEDIR/python2/py278/lib
+./configure --prefix=$HOMEDIR/python2/py278 --enable-shared
 
 make
-cd /opt/lastlist/apps/setup/python/Python-2.7.8
+cd /opt/bchlabs/apps/setup/python2/Python-2.7.8
 make install
